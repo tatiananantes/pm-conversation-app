cefeb5f928fbef0f65ea56f8f4f5ee23edd20012
tatiananantes <tatiananantes@gmail.com>
2023-03-10 14:22:43 +0000

    projects are created with correct default status

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,27 +1,28 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_project, only: %i[ show edit update destroy ]
 
-  # GET /projects or /projects.json
+  # lists all existing projects
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1 or /projects/1.json
+  # shows a specific project and its details
   def show
   end
 
-  # GET /projects/new
+  # creates a new project
   def new
-    @project = Project.new
+    @project = current_user.projects.build
   end
 
-  # GET /projects/1/edit
+  # edits a project
   def edit
   end
 
   # POST /projects or /projects.json
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.build(project_params)
 
     respond_to do |format|
       if @project.save
@@ -58,13 +59,13 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_project
-      @project = Project.find(params[:id])
+      @project = current_user.projects.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:name, :description, :user_id)
+      params.require(:project).permit(:name, :status, :description)
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,12 @@
 class Project < ApplicationRecord
+  has_many :comments
   belongs_to :user
+  before_create :set_default_status
+
+  private
+
+  def set_default_status
+    self.status ||= "Created"
+  end
+
 end
