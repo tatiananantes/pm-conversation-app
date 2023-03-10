3c599ca3c03bbeb05a6a52273b6b4cafb5a403f4
tatiananantes <tatiananantes@gmail.com>
2023-03-10 13:50:14 +0000

    add status to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_134008) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_134923) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_134008) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
